e97d64126397d38adb1c1ae11495cba4247974ee
Whitney Seiler <whitney.seiler@gmail.com>
2018-09-05 21:39:18 -0700

    add mouse event functions to highlight hovered state and display info

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'Axios';
+import Search from './Search.jsx';
+import Map from './Map.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,14 +10,58 @@ class App extends React.Component {
     this.state = {
     }
 
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+
   }
 
   /*
   * retrieve top 20 trending GIFs upon component mount
   */
   componentDidMount() {
-    this.fetchGIFs('trending')
+    
   }
+
+  handleMouseEnter(e) {
+    e.target.style.fill = "purple"
+    
+    let infoBox = document.getElementById('info-box');
+    let info = e.target.getAttribute('data-info');
+    infoBox.style.display = "block";
+
+    infoBox.innerHTML = info;
+  }
+  
+  handleMouseLeave(e) {
+    let infoBox = document.getElementById('info-box');
+    infoBox.style.display = "none";
+    e.target.style.fill = "";
+  }
+
+  handleMouseMove(e) {
+    let infoBox = document.getElementById('info-box');
+    let top = e.clientY - 80;
+    let left = e.clientX;
+
+    infoBox.style.position = "absolute";
+    infoBox.style.top = `${top}px`;
+    infoBox.style.left = `${left}px`;
+    infoBox.style.backgroundColor = '#ffffff';
+    infoBox.style.border = '2px solid #ee6e73';
+    infoBox.style.borderRadius = '5px';
+    infoBox.style.padding = '5px';
+    infoBox.style.fontFamily = 'Raleway, sans-serif';
+  }
+  
+  // var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  // if (ios) {
+  //   $('a').on('click touchend', function() {
+  //     var link = $(this).attr('href');
+  //     window.open(link,'_blank');
+  //     return false;
+  //   });
+  // }
 
   /**
    * when user enters search query and hits enter, reset offset to 0 
@@ -44,6 +90,7 @@ class App extends React.Component {
           </nav>
         </header>
         <section >
+          <Map handleMouseEnter={this.handleMouseEnter} handleMouseLeave={this.handleMouseLeave} handleMouseMove={this.handleMouseMove}/>
         </section>
       </main>
     )
