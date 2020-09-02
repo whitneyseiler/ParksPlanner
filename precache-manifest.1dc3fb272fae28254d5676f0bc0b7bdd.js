50b4701e388ef2185da50b0f1e1ca646e43a8128
Whitney Seiler <whitney.seiler@gmail.com>
2020-09-01 18:18:36 -0700

    Updates

---
+++ precache-manifest.1dc3fb272fae28254d5676f0bc0b7bdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9055689cf40f87d233d2a00dd3533d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4383691a1133141ce342",
+    "url": "/static/js/2.68a81ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68a81ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52b1560f5c107036a3f",
+    "url": "/static/js/main.5ccaa401.chunk.js"
+  },
+  {
+    "revision": "6043c571f64e28428952",
+    "url": "/static/js/runtime-main.0f836afc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ParksPlanner/precache-manifest.bbaec2ff9badc53c06b75539a6922011.js"
+  "/precache-manifest.1dc3fb272fae28254d5676f0bc0b7bdd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ParksPlanner/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
