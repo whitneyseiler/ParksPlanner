f0d8a770beccfeacafbfa9cecbc2e532d5da9ed7
Whitney Seiler <whitney.seiler@gmail.com>
2020-09-01 21:58:13 -0700

    pass clearSearch to Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Row, Input, Button} from 'react-materialize'
 
-let Search = ({handleKeyDown}) => {
+let Search = ({clearSearch}) => {
     return (
 			<div className="nav-wrapper">
 				<form>
 					<div className="input-field">
-						<input id="feature-filter" type="search" />
+						<input id="feature-filter" type="search" autoComplete="off" />
 						<label className="label-icon" htmlFor="search">
 							<i className="material-icons">search</i>
 						</label>
-						<i className="material-icons">close</i>
+						<i className="material-icons" onClick={() => clearSearch()}>close</i>
 					</div>
 				</form>
 			</div>
