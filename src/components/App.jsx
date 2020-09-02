028be0108b833264d6f52f4cbe755e7cc89ec11d
Whitney Seiler <whitney.seiler@gmail.com>
2020-09-01 20:39:41 -0700

    add Search to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Map from './Map.jsx';
+import Search from './Search.jsx';
+import { listenEvents } from 'react-mapbox-gl/lib/map-events';
 
 let App = () => {
   return (
@@ -7,7 +9,9 @@ let App = () => {
       <header className="header">
         <h1 className="brand-logo center" onClick={() => window.location.reload()}>Parks Planner</h1>
         <h4>“I haven’t been everywhere, but it’s on my list.” - Susan Sontag</h4>
-        <nav></nav>
+        <nav>
+          <Search />
+        </nav>
       </header>
       <Map/>
     </main>
